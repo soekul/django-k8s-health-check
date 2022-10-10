b60d2afc04c7c6e1a11a18f8f8bb8743f04900aa
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2022-10-10 18:08:29 -0300

    Bug fix: xenon complexity

--- django_k8s_health_check/management/commands/health_check.py
+++ django_k8s_health_check/management/commands/health_check.py
@@ -7,15 +7,21 @@ from django_k8s_health_check.utils import HealthCheck
 class Command(BaseCommand):
     help = 'Check databases and caches connection'
 
+    def test_database(self):
+        db_error, _ = HealthCheck.test_databases()
+        if db_error:
+            raise Exception('Database connection error')
+    
+    def test_cache(self):
+        cache_error, _ = HealthCheck.test_caches()
+        if cache_error:
+            raise Exception('Cache connection error')
+
     def handle(self, *args, **options):  # pylint: disable=unused-argument
         if api_settings.CHECK_DATABASE:
-            db_error, _ = HealthCheck.test_databases()
-            if db_error:
-                raise Exception('Database connection error')
+            self.test_database()
 
         if api_settings.CHECK_CACHE:
-            cache_error, _ = HealthCheck.test_caches()
-            if cache_error:
-                raise Exception('Cache connection error')
+            self.test_cache()
 
         self.stdout.write('Databases and caches ok')
