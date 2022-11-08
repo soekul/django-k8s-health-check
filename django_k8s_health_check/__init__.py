c046d71cf5301154617863a543845f799e1bdee2
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2022-11-08 09:41:46 -0300

    Fix for old version of django

--- django_k8s_health_check/__init__.py
+++ django_k8s_health_check/__init__.py
@@ -5,6 +5,6 @@ except ImportError:
     django = None
 
 if django and django.VERSION < (3, 2):  # pragma: no cover
-    default_app_config = 'django_k8s_health_check.apps.DjangoHealthCheck'
+    default_app_config = 'django_k8s_health_check.apps.DjangoK8sHealthCheck'
 
 __version__ = '1.1.1'
